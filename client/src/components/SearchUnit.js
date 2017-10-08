9216166733d1d313fb35c52be5f8c95e0345410d
Michael Gutleber <michaeljgut@gmail.com>
2017-10-08 10:01:47 -0400

    Started adding select to Search Unit component

--- client/src/components/SearchUnit.js
+++ client/src/components/SearchUnit.js
@@ -6,19 +6,30 @@ class SearchUnit extends Component {
     super();
     this.state = {
       query: '',
+      topic: '',
       definition: [],
       fireRedirect: false,
     };
     this.getAPIData = this.getAPIData.bind(this);
+    this.handleChange = this.handleChange.bind(this);
+    this.handleSubmit = this.handleSubmit.bind(this);
+  }
+
+  handleChange(event) {
+    this.setState({value: event.target.value});
+  }
+
+  handleSubmit(event) {
+    alert('Your favorite flavor is: ' + this.state.value);
+    event.preventDefault();
   }
 
   componentDidMount() {
-    this.getAPIData();
+//    this.getAPIData();
   }
 
-  getAPIData() {
-//    e.preventDefault();
-    console.log(process.env);
+  getAPIData(e) {
+    e.preventDefault();
     let getQuery = 'https://api.nytimes.com/svc/search/v2/articlesearch.json?api-key=' +
       process.env.REACT_APP_ARTICLES_API_KEY
     axios
@@ -40,7 +51,35 @@ class SearchUnit extends Component {
   }
 
   render(){
-    return <h1>Search Unit</h1>;
+    return (
+      <div className="get-articles">
+        <h1>NY Times Article Search</h1>;
+        <form onSubmit={this.handleFormSubmit}>
+          <input
+            type="text"
+            placeholder="Query"
+            name="query"
+            value={this.state.query}
+            onChange={this.handleInputChange}
+            autoFocus
+          />
+          <label>
+            Pick a NY Times Subject Areakey: "value",
+            <select value={this.state.value} onChange={this.handleChange}>
+              <option value="grapefruit">Grapefruit</option>
+              <option value="lime">Lime</option>
+              <option value="coconut">Coconut</option>
+              <option value="mango">Mango</option>
+            </select>
+          </label>
+            <input className='submit' type="submit" value="SUBMIT" />
+        </form>
+        <button onClick={this.cancelFlashcard}>Cancel</button>
+        {this.state.fireRedirect
+          ? <Redirect push to={pathSubject} />
+          : ''}
+      </div>
+      )
   }
 }
 
